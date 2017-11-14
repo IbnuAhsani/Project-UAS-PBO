724f1670aed54513af163fed4fffd4003d30d26d
faizinahsan <faizin110998@gmail.com>
2017-11-14 19:34:39 +0700

    Dokumentasi Language Class

--- icu-transliteration-application-master/src/googletranslate/Language.java
+++ icu-transliteration-application-master/src/googletranslate/Language.java
@@ -1,16 +1,17 @@
 package googletranslate;
-
+// Menginisialisasikan Language sebagai enum yang datanya bersifat konstan
 public enum Language
 {
+	// Meninisialisasikan variabel Enum dengan value, NAMAENUM("VALUE")
   AUTO_DETECT(""),  ALBANIAN("sq"),  ARABIC("ar"),  BULGARIAN("bg"),  CATALAN("ca"),  CHINESE("zh"),  CHINESE_SIMPLIFIED("zh-CN"),  CHINESE_TRADITIONAL("zh-TW"),  CROATIAN("hr"),  CZECH("cs"),  DANISH("da"),  DUTCH("nl"),  ENGLISH("en"),  ESTONIAN("et"),  FILIPINO("tl"),  FINNISH("fi"),  FRENCH("fr"),  GALACIAN("gl"),  GERMAN("de"),  GREEK("el"),  HEBREW("iw"),  HINDI("hi"),  HUNGARIAN("hu"),  INDONESIAN("id"),  ITALIAN("it"),  JAPANESE("ja"),  KOREAN("ko"),  LATVIAN("lv"),  LITHUANIAN("lt"),  MALTESE("mt"),  NORWEGIAN("no"),  PERSIAN("fa"),  POLISH("pl"),  PORTUGUESE("pt"),  ROMANIAN("ro"),  RUSSIAN("ru"),  SERBIAN("sr"),  SLOVAK("sk"),  SLOVENIAN("sl"),  SPANISH("es"),  SWEDISH("sv"),  THAI("th"),  TURKISH("tr"),  UKRANIAN("uk"),  VIETNAMESE("vi");
-  
+  // Membuat variabel konstanta dengan tipe string
   private final String language;
-  
+  // Konstruktor Language dengan parameter ???
   private Language(String paramString)
   {
     this.language = paramString;
   }
-  
+  // Mengembalikan value dari language
   public String toString()
   {
     return this.language;
